330ab80bcab7e5385c172ce01857f959f10cc6dd
Estra-Dev <dominikolyson@gmail.com>
2025-09-26 01:06:37 +0100

    centralize

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='flex h-screen w-screen items-center justify-center overflow-hidden'>
-      <h1 className=' font-bold text-2xl text-gray-700'>Car <span className=' text-lime-500'>Maintenance</span></h1>
+      <h1 className=' font-bold text-2xl text-gray-700 text-center'>Car <span className=' text-lime-500'>Maintenance</span></h1>
       <div className="car-animation">
         <Car className=' w-32 h-32 text-lime-500' />
       </div>
